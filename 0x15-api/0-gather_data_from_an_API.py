61044b2c7d9f414e77ab6b1b098702eaf126d8a0
Domengo <dominicsengo@gmail.com>
2023-02-09 02:36:11 +0300

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import requests
+from sys import argv
+
+
+def get_todo_list(user_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+    response = requests.get(url)
+    if response.status_code == 200:
+        todo_list = response.json()
+        done_tasks = [task for task in todo_list if task["completed"] is True]
+        done_tasks_count = len(done_tasks)
+        total_tasks_count = len(todo_list)
+        name = todo_list[0]["userId"]
+        employee_name = f"Employee {name}"
+        print(f"{employee_name} is done with tasks\
+              ({done_tasks_count}/{total_tasks_count}): ")
+
+        for task in done_tasks:
+            print(f"\t {task['title']}")
+    else:
+        print("An error has occurred while trying to retrieve the TODO list")
+
+
+employee_id = argv[1]
+get_todo_list(employee_id)
