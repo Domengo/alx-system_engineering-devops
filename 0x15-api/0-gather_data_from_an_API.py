43cf8f469dfdb9ca4308af047d9ffb7c69f9ae6b
Domengo <dominicsengo@gmail.com>
2023-02-09 03:11:22 +0300

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,6 +7,7 @@ Implemented using recursion
 import requests
 from sys import argv
 
+
 def get_todo_list(user_id):
     url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
     response = requests.get(url)
@@ -19,11 +20,13 @@ def get_todo_list(user_id):
         done_tasks = [task for task in todo_list if task["completed"] is True]
         done_tasks_count = len(done_tasks)
         total_tasks_count = len(todo_list)
-        print(f"Employee {employee_name} is done with tasks({done_tasks_count}/{total_tasks_count}):")
+        print(f"Employee {employee_name} is done with tasks\
+            ({done_tasks_count}/{total_tasks_count}):")
         for task in done_tasks:
             print(f"\t {task['title']}")
     else:
         print("An error has occurred while trying to retrieve the TODO list")
+
 
 if __name__ == '__main__':
     employee_id = argv[1]
