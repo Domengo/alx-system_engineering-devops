ad51923e88012f96223bb8f6b7e8b0a3d5235274
Domengo <dominicsengo@gmail.com>
2023-02-09 03:10:16 +0300

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,25 +7,23 @@ Implemented using recursion
 import requests
 from sys import argv
 
-
 def get_todo_list(user_id):
-    url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+    url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
     response = requests.get(url)
     if response.status_code == 200:
+        employee = response.json()
+        employee_name = employee["name"]
+        url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+        response = requests.get(url)
         todo_list = response.json()
         done_tasks = [task for task in todo_list if task["completed"] is True]
         done_tasks_count = len(done_tasks)
         total_tasks_count = len(todo_list)
-        name = todo_list[0]["userId"]
-        employee_name = f"Employee {todo_list.get('name')}"
-        print(f"{employee_name} is done with tasks\
-              ({done_tasks_count}/{total_tasks_count}): ")
-
+        print(f"Employee {employee_name} is done with tasks({done_tasks_count}/{total_tasks_count}):")
         for task in done_tasks:
             print(f"\t {task['title']}")
     else:
         print("An error has occurred while trying to retrieve the TODO list")
-
 
 if __name__ == '__main__':
     employee_id = argv[1]
