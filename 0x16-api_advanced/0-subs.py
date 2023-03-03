4b965077a1d02161ed9d985c0bf4715c0e369b9e
Domengo <dominicsengo@gmail.com>
2023-03-04 02:35:32 +0300

    pep8 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and returns the
-number of subscribers (not active users, total subscribers)
-for a given subreddit. If an invalid subreddit is given,
-the function should return 0.
-    """
+"""function that queries the Reddit API"""
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the
+    number of subscribers for a given subreddit.
+
+    Args:
+    subreddit (str): The subreddit to query.
+
+    Returns:
+    int: The number of subscribers for the given subreddit.
+    If the subreddit is invalid, returns 0.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
