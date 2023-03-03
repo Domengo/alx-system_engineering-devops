30fdd010f19bba5c7bb8283fdd1ec0d8d1806fbc
Domengo <dominicsengo@gmail.com>
2023-03-04 02:02:38 +0300

    subredit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+import requests
+"""function that queries the Reddit API and returns the
+number of subscribers (not active users, total subscribers)
+for a given subreddit. If an invalid subreddit is given,
+the function should return 0.
+    """
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
