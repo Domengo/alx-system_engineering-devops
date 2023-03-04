0814dc7b084c52181080b7d3fcc2bfe637e4fd0e
Domengo <dominicsengo@gmail.com>
2023-03-04 03:03:07 +0300

    top 10

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,7 +16,7 @@ def number_of_subscribers(subreddit):
     If the subreddit is invalid, returns 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'User Agent'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import requests
+"""_summary_
+    """
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API andbprints the titles of the
+    first 10 hot posts listed for a given subreddit
+
+    Args:
+    subreddit (str): The subreddit to query.
+
+    Returns:
+    str: The title for the given subreddit.
+    If the subreddit is invalid, returns None.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'User Agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    else:
+        print(None)
